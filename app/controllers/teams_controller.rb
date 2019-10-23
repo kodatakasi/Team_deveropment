dea1c1c5288b3e05bba6cd12f7e50e9c6141058f
koda takasi <kodatakasi@gmail.com>
2019-10-23 18:30:06 +0900

    権限移動ボタン作成

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -47,6 +47,11 @@ class TeamsController < ApplicationController
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
   end
 
+  def change
+    team = Team.find(current_user.keep_team_id)
+    team.update(owner_id: change_owner_id)
+  end
+
   private
 
   def set_team
